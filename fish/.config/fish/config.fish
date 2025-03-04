2729797ff107ac5158f28896c5bd687312708147
WillDyer <will.dyer9@gmail.com>
2025-03-04 12:31:43 +0000

    fixes

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -21,5 +21,13 @@ set -Ux PYTHONPATH "/usr/bin/python3.12:/usr/autodesk/maya2024/lib/python3.10/si
 set -Ux PATH "/usr/autodesk/maya2024/bin/mayapy" $PATH
 # set -Ux PYTHONPATH "/usr/bin/python3.12" $PYTHONPATH
 
+# Start ssh-agent if not running
+if not pgrep -u (whoami) ssh-agent >/dev/null
+    eval (ssh-agent -c)
+end
 
-
+# Add SSH key if it's not already added
+ssh-add -l >/dev/null 2>&1
+if test $status -ne 0
+    ssh-add ~/.ssh/id_rsa 2>/dev/null
+end
